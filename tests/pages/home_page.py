cd6c48f26498d0b582d952c791576cbedd489e7d
LaidonerS <stenlaidoner@gmail.com>
2025-11-22 23:29:31 +0700

    Expose session activity elements on HomePage POM

--- tests/pages/home_page.py
+++ tests/pages/home_page.py
@@ -6,31 +6,20 @@ class HomePage:
         self.page = page
         self.base_url = base_url
 
-    def goto_activity_section(self):
-        self.page.goto(self.base_url + "#activity")
-
-    @property
-    def activity_card(self):
-        return self.page.get_by_test_id("scenario-activity")
-
-    @property
-    def activity_load_button(self):
-        return self.page.get_by_test_id("load-activity")
-
-    @property
-    def activity_status(self):
-        return self.page.get_by_test_id("activity-status")
-
-    @property
-    def activity_items(self):
-        return self.page.get_by_test_id("activity-item")
-
-
     # Navigation
     def goto(self):
         self.page.goto(self.base_url)
 
-    # Elements
+    def goto_products_section(self):
+        self.page.goto(self.base_url + "#products")
+
+    def goto_contact_section(self):
+        self.page.goto(self.base_url + "#contact")
+
+    def goto_activity_section(self):
+        self.page.goto(self.base_url + "#activity")
+
+    # Top-level elements
     @property
     def heading(self):
         return self.page.get_by_test_id("page-title")
@@ -47,9 +36,28 @@ class HomePage:
     def contact_link(self):
         return self.page.get_by_role("link", name="Contact")
 
-    # Sections
-    def goto_products_section(self):
-        self.page.goto(self.base_url + "#products")
+    # Activity feed (remote API)
+    @property
+    def activity_card(self):
+        return self.page.get_by_test_id("scenario-activity")
 
-    def goto_contact_section(self):
-        self.page.goto(self.base_url + "#contact")
+    @property
+    def activity_load_button(self):
+        return self.page.get_by_test_id("load-activity")
+
+    @property
+    def activity_status(self):
+        return self.page.get_by_test_id("activity-status")
+
+    @property
+    def activity_items(self):
+        return self.page.get_by_test_id("activity-item")
+
+    # Session activity log
+    @property
+    def session_activity_list(self):
+        return self.page.get_by_test_id("session-activity-list")
+
+    @property
+    def session_activity_items(self):
+        return self.page.get_by_test_id("session-activity-item")
